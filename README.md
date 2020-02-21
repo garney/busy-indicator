8173bf3a434792d59c758ba24a7571ef0299dd04
Garney <garney.andres@3plearning.com>
2020-02-21 13:56:18 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+busy-indicator
+
+Busy indicator that wraps react-loader-spinner
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```bash
+npm i busy-indicator
+```
+
+## ✨ Usage
+
+```javascript
+
+```
+
+## 📌 Props
+
+Prop                  | Type     | Default                   | Required
+--------------------- | -------- | ------------------------- | --------
+`options`|any|`false`|No
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, Fragment } from 'react';
+
+import Loader from 'react-loader-spinner';
+import EventDispatcher from '@garney/event-dispatcher';
+
+
+class BusyDispatcher extends EventDispatcher {
+  constructor() {
+    super();
+    this.state = false;
+  }
+
+  setBusyStatus(status) {
+    this.state = status;
+    this.dispatchEvent(BusyDispatcher.events.SET_BUSY_STATUS, this.state);
+  }
+
+  get status() {
+    return this.state;
+  }
+}
+
+BusyDispatcher.instance = new BusyDispatcher();
+BusyDispatcher.events = {
+  SET_BUSY_STATUS: "SET_BUSY_STATUS"
+};
+function BusyIndicator({ options = {} }) {
+  const defaultOptions = {
+    type: 'Puff',
+    color: '#00BFFF',
+    height: 50,
+    width: 50
+  };
+ const busyStyle =  {
+      'background-color': 'rgba(0,0,0,.5)',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      'justify-content': 'center',
+      'align-items': 'center'
+    };
+
+  options = {
+    ...defaultOptions,
+    ...options
+  };
+
+  const { type, color, height, width } = options;
+  const [status, setStatus] = useState(BusyDispatcher.instance.status);
+
+  useEffect(() => {
+    BusyDispatcher.instance.addEventListener(BusyDispatcher.events.SET_BUSY_STATUS, (status) => {
+      setStatus(status);
+    })
+  }, []);
+
+  const view = status ? (
+      <div style={busyStyle}>
+        <Loader
+            type={type}
+            color={color}
+            height={height}
+            width={width}
+        />
+      </div>
+  ): '';
+  return view;
+}
+
+function setBusyStatus(status) {
+  BusyDispatcher.instance.setBusyStatus(status)
+}
+
+
+
+
+module.exports = {
+  BusyIndicator,
+  setBusyStatus,
+  status: BusyDispatcher.instance.status
+};
